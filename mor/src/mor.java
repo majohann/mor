a5c0378b25d42c98018a5bb50f199d4bf7e9db2b
majohann <majo_rabaza@hotmail.com>
2016-01-18 21:42:26 -0300

    CONSTRUCCIÓN SOLUCIÓN FACTIBLE (Versión aparentemente funcionando)

--- mor/src/mor.java
+++ mor/src/mor.java
@@ -21,12 +21,12 @@ public class mor {
 				
 		//Cargo nodos terminales (después deberíamos cargarlos desde un archivo)(por ahora harcodeado)
 		List<Integer> T = new ArrayList<Integer>();
-		T.add(1);
-		T.add(5);
+		T.add(0);
+		T.add(4);
 		
 		//al momento de cargar los T debo generar esto (por ahora harcodeado)
 		Map<Pair<Integer, Integer>, Integer> m_ij = new HashMap<Pair<Integer,Integer>, Integer>();
-		m_ij.put(new Pair<Integer, Integer>(1,5),3);
+		m_ij.put(new Pair<Integer, Integer>(0,4),3);
 		//m_ij.put(new Pair<Integer, Integer>(5,1),3);	
 		
 		//Creo Pij lista de caminos nodos-disjuntos
@@ -35,7 +35,7 @@ public class mor {
 		//Creo grafo Gsol cuyos nodos son los terminales y no tiene aristas		
 		Graph Gsol = new Graph();
 		Gsol.set_vertex_to_graph(T);	
-		Gsol.export_to_file("data/salidas/Gsol_T.txt");
+		//Gsol.export_to_file("data/salidas/Gsol_T.txt");
 		
 		//seleccionar randomicamente i,j in TxT /mij>0
 		Pair<Integer,Integer> random_ij = get_random_ij(m_ij,T,1);
@@ -49,22 +49,23 @@ public class mor {
 				H = H.grafo_menos_camino(p);
 			}
 
-			Map<Pair<Integer, Integer>, Double> G_edges_cost = G.get_vertex_pair_weight_index();
+			Map<Pair<Integer, Integer>, Double> cost_techo = G.get_vertex_pair_weight_index();
 			Map<Pair<Integer, Integer>, Double> Gsol_edges_cost = Gsol.get_vertex_pair_weight_index();
 			if (Gsol_edges_cost==null)
 				Gsol_edges_cost = new HashMap<Pair<Integer,Integer>, Double>();
 			for (Entry<Pair<Integer, Integer>, Double> entry : Gsol_edges_cost.entrySet()) {
 				Pair<Integer, Integer> pair = entry.getKey();			    
-			    Gsol_edges_cost.put(pair, (double) 0);			    
+				cost_techo.put(pair, (double) 0);			    
 			}
 			
-			H.set_vertex_pair_weight_index(Gsol_edges_cost);
+			H.set_vertex_pair_weight_index(cost_techo);
 			
 			//Yen
 			YenTopKShortestPathsAlg yen = new YenTopKShortestPathsAlg(H);
 			List<Path> yens_path = yen.get_shortest_paths(H.get_vertex(random_ij.first()),H.get_vertex(random_ij.second()), 3);
 			
 			if ((yens_path!=null)&&(yens_path.size()>0)){
+				
 				Path shortest_path = yens_path.get(0); //VERRRRRRRR
 				Vertex i = (Vertex) H.get_vertex(random_ij.first());
 				Vertex j = (Vertex) H.get_vertex(random_ij.second());
@@ -78,45 +79,29 @@ public class mor {
 					Random randomizer = new Random();
 					//int old_mij = m_ij.get(new Pair<Integer,Integer>(i.get_id(),j.get_id()));
 					//m_ij.put(new Pair<Integer,Integer>(i.get_id(),j.get_id()),old_mij-1);
-					Path p_random = yens_path.get(randomizer.nextInt(yens_path.size()-1));
+					
+					Path p_random = null;
+					if (yens_path.size()==1){
+						p_random = yens_path.get(0);
+					}else {
+						p_random = yens_path.get(randomizer.nextInt(yens_path.size()-1));
+					}
 					//i = (Vertex) p_random.get_vertices().get(0);
 					//j = (Vertex) p_random.get_vertices().get(p_random.get_vertices().size()-1);
 					//int old_mij = m_ij.get(new Pair<Integer,Integer>(i.get_id(),j.get_id()));
 					//m_ij.put(new Pair<Integer,Integer>(i.get_id(),j.get_id()),old_mij-1);
 					Pij.add(p_random);		
-					Gsol.grafo_mas_camino(p_random, G);
+					Gsol = Gsol.grafo_mas_camino(p_random, G);
+					Gsol.export_to_file("data/salidas/Gsol_iter_"+iter+".txt");
 				}
 			}	
 			
-			Gsol.export_to_file("data/salidas/H_iter_"+iter+".txt");
+			//Gsol.export_to_file("data/salidas/H_iter_"+iter+".txt");
 			iter++;
 			random_ij = get_random_ij(m_ij,T,1);
 		}
 		
-		H.export_to_file("data/salidas/G_solucion.txt");
-		
-		
-		
-//		Graph G_copy = G.copy_of_graph();
-//		G_copy.export_to_file("data/salidas/G_copy.txt");
-//		
-/*		List<BaseVertex> vertex_list = new ArrayList<BaseVertex>();
-		BaseVertex v =  new Vertex();
-		v.set_id(0);
-		v.set_weight(1);
-		vertex_list.add(v);
-		v =  new Vertex();
-		v.set_id(1);
-		v.set_weight(1);
-		vertex_list.add(v);
-		
-		Path p = new Path(vertex_list,3);
-		
-		Gsol = Gsol.grafo_mas_camino(p, G);
-		Gsol.export_to_file("data/salidas/G_mas_camino.txt");*/
-//	
-//		Graph G_menos_camino = G_copy.grafo_menos_camino(p);
-//		G_menos_camino.export_to_file("data/salidas/G_menos_camino.txt");
+		Gsol.export_to_file("data/salidas/G_solucion.txt");
 		
 		System.out.println("Fin.");
 		
